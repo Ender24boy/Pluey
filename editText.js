1cf1713da230b3018cedf94e92f99ca68051895c
Ender24Boy <nrmyny@gmail.com>
2025-03-03 07:31:38 -0500

    updates permissions and fixed bug regarding typecase

--- editText.js
+++ editText.js
@@ -1,11 +1,10 @@
 function editText(text, message) {
-    const finalMessageREGEX = message.toLowerCase()
 
     var x = 0
     var finalMessage = ''
     for (let i = 0; i < text.length; i++) {
         var currentLetter = text[i]
-        if (currentLetter == finalMessageREGEX[x]){
+        if (currentLetter == message[x]){
             finalMessage = finalMessage + currentLetter
             x = x + 1
         } else {
